2e7dbd1198837ee66f4a6cd5927ba7586eeecc6c
mbrown <tugtuj@gmail.com>
2021-03-09 11:44:50 -0500

    New post page add

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router-dom'
 import Home from '../components/Home'
 import Gardens from '../components/Gardens'
+import NewPost from '../components/NewPost'
 
 
 export default(
     <Switch>
         <Route exact path='/' component={ Home } />
         <Route exact path='/gardens' component={ Gardens } />
+        <Route exact path='/newpost' component={ NewPost } />
     </Switch> 
 )
