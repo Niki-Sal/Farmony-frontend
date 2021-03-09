9be5fc9ad06bd0aae276c3108697ca20aa0c96ab
mbrown <tugtuj@gmail.com>
2021-03-09 14:05:18 -0500

    EditForm route add

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../components/Home'
 import Gardens from '../components/Gardens'
 import NewPost from '../components/NewPost'
+import EditForm from '../components/EditForm'
 
 
 export default(
@@ -9,5 +10,6 @@ export default(
         <Route exact path='/' component={ Home } />
         <Route exact path='/gardens' component={ Gardens } />
         <Route exact path='/newpost' component={ NewPost } />
+        <Route exact path='/editform' component={ EditForm } />
     </Switch> 
 )
