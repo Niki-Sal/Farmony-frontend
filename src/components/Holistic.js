e38672476885ae210e016303f5cc3905cd0a8f8c
mbrown <tugtuj@gmail.com>
2021-03-09 11:44:21 -0500

    Page setup - showing posts, make post, view post

---
+++ src/components/Holistic.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import NewPost from './NewPost'
+import HolisticModel from '../models/holistic'
+
+const Holistic = () => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+           const res = await HolisticModel.all()
+           console.log(res)
+           setPosts(res.data)
+        };
+        fetchData()
+    }, []);
+    const listOfPosts = posts.map((post) => {
+        return (
+            <div key={post._id}>{post.title}{post.username}{post.date}<button>View Post</button></div>
+        )
+    })
+
+    return (
+        <div>
+            <h1>Holistic Hub</h1>
+            <h4>Community chit-chat. Discuss community news, trade recipes, share photos, etc. Be kind and respectful. </h4>
+            <button><Link to="/newpost">Make New Post</Link></button>
+            {listOfPosts}
+            
+        </div>
+        
+    );
+}
+
+export default Holistic;
