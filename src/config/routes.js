bf1becf2db51e35659170e7323b9588c88c536f8
mbrown <tugtuj@gmail.com>
2021-03-09 17:30:09 -0500

    ViewPost, Holistic route Add

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,8 @@ import Home from '../components/Home'
 import Gardens from '../components/Gardens'
 import NewPost from '../components/NewPost'
 import EditForm from '../components/EditForm'
+import ViewPost from '../components/ViewPost'
+import Holistic from '../components/Holistic'
 
 
 export default(
@@ -11,5 +13,6 @@ export default(
         <Route exact path='/gardens' component={ Gardens } />
         <Route exact path='/newpost' component={ NewPost } />
         <Route exact path='/editform' component={ EditForm } />
+        <Route exact path='/viewpost' component={ ViewPost } />
     </Switch> 
 )
