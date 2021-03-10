5e4baba4ef543b58cdbb2a04970deb311fbb658e
mbrown <tugtuj@gmail.com>
2021-03-10 00:46:21 -0500

    UseEffect add

--- src/components/ViewPost.js
+++ src/components/ViewPost.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HolisticModel from '../models/holistic'
+
 
 const ViewPost = (props) => {
 
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await HolisticModel.all()
+            console.log(res.data) 
+            //setPosts(res.data) 
+        };
+        fetchData()
+    }, []);
     
     console.log(props.id)
 
     return (
         <div>
-            <p>post {props.id}</p>
+            <p>post {posts}{props.id}</p>
         </div>
     );
 }
