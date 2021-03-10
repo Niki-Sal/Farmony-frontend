9d0e593537d21dc38457d05f89849d6aed79b109
mbrown <tugtuj@gmail.com>
2021-03-09 19:22:05 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Welcome from './components/Welcome';
 import Home from './components/Home';
 import Holistic from './components/Holistic';
 
+
 const PrivateRoute = ({ component: Component, ...rest}) => {
   let token = localStorage.getItem('jwtToken');
   console.log('===> Hitting a Private Route');
