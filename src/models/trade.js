66be83fdbecfc88ce80c5b60f8778544090cffa6
mbrown <tugtuj@gmail.com>
2021-03-10 23:47:50 -0500

    File add

---
+++ src/models/trade.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const endPoint = 'http://localhost:8000/api/trade'
+
+
+
+class TradeModel {
+    
+    static all = () => {
+        let request = axios.get(endPoint)
+        return request
+    }
+    static create = (post)=>{
+        let request = axios.post(endPoint, post)
+        return request
+    }
+    static delete = (post) =>{
+        let request = axios.delete( )
+        return request
+    }
+    static show = (post) => {
+        let request = axios.show(`${endPoint}/${post._id}`)
+        return request
+    }
+    
+}
+
+export default TradeModel;
