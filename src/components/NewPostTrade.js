b7f5ba9d5b4daba586807415404d39bcacf976b8
mbrown <tugtuj@gmail.com>
2021-03-11 01:04:12 -0500

    File add

---
+++ src/components/NewPostTrade.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import TradeModel from '../models/trade'
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import ViewPostTrade from './ViewPostTrade';
+
+const NewPostTrade = () => {
+    const [category, setCategory] = useState('')
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [postType, setPostType] = useState('')
+    const [currentUser, setCurrentUser] = useState({});
+    const [isAuthenticated, setIsAuthenticated] = useState(true);
+    
+    useEffect(() => {
+      let token;
+  
+      if (!localStorage.getItem('jwtToken')) {
+        setIsAuthenticated(false);
+        console.log('====> Authenticated is now FALSE');
+      } else {
+        token = jwt_decode(localStorage.getItem('jwtToken'));
+        setAuthToken(localStorage.getItem('jwtToken'));
+        setCurrentUser(token);
+      }
+    }, []);
+
+
+
+    const handleTitle = (e) => {
+        setTitle(e.target.value)
+        console.log('***** title', title)
+    }
+
+    const handleBody = (e) => {
+        setBody(e.target.value)
+        console.log('***** body', body)
+    }
+
+    const handleCategory = (e) => {
+        setCategory(e.target.value)
+        console.log('***** category', category)
+    }
+
+    const handlePostType = (e) => {
+        setPostType(e.target.value)
+        console.log('***** post type', postType)
+    }
+
+    const onFormSubmit = (e) => {
+        //e.preventDefault()
+        console.log(title, body, category, currentUser.name, postType)
+        // if (title || body || category|| postType === '') {
+        //     alert('Must fill out all fields.')
+        // }
+        alert('Post Submited! :)')
+        TradeModel.create({
+            title,
+            name: currentUser.name,
+            photo: currentUser.photo,
+            body,
+            postType,
+            category,
+        })
+
+        
+    }
+    
+    
+
+    return (
+        <div>
+                <form onSubmit={onFormSubmit}>
+                    <label>
+                        Post Title: 
+                        <input type="text" name="postTitle" value={title} onChange={handleTitle}></input>
+                    </label><br />
+                    <label>Body: 
+                    <input type="text" name="body" value={body} onChange={handleBody}></input>
+                    </label><br/>
+                    <label>
+                        Category:
+                        <select value={category} onChange={handleCategory}>
+                        <option value="Trade">Trade</option>
+                        <option value="Buy">Buy</option>
+                        <option value="Volunteer">Volunteer</option>
+                        <option value="Trade Hub">Trade Hub</option>
+                        </select>
+                        Post Type: 
+                        <select value={postType} onChange={handlePostType}>
+                        <option value="Seeking">Seeking</option>
+                        <option value="Sharing">Sharing</option>
+                        </select>
+                        
+                        <input type="submit" value="Submit"></input>
+                        
+                    </label>
+
+                </form>
+        </div>
+    );
+}
+
+export default NewPostTrade;
