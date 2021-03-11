aab86d89a32786399da8db36c78b355523790111
mbrown <tugtuj@gmail.com>
2021-03-11 16:39:27 -0500

    Edits

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import NewPost from './NewPost'
-import ViewPost from './ViewPost'
 import TradeModel from '../models/trade'
 
 
