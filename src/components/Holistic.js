b20f060a5663963c32cf35ff9ccafe4abf504776
mbrown <tugtuj@gmail.com>
2021-03-10 21:43:58 -0500

    route updated

--- src/components/Holistic.js
+++ src/components/Holistic.js
@@ -20,7 +20,7 @@ const Holistic = () => {
 
     const listOfPosts = posts.map((post) => {
         return (
-            <div key={post._id}>{post.title}{post.username}{post.date}<button><Link to="/viewpost/:id" id={post._id}>View Post</Link></button></div>
+            <div key={post._id}>{post.title}{post.name}{post.date}<button><Link to="/post/:id">View Post</Link></button></div>
         )
     })
 
