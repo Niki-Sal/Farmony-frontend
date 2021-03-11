823501547d4f97de0c4cc8d535754344b62decf9
mbrown <tugtuj@gmail.com>
2021-03-11 01:02:50 -0500

    Corrected routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,8 @@ import NewPost from '../components/NewPost'
 import EditForm from '../components/EditForm'
 import ViewPost from '../components/ViewPost'
 import GardenProfile from '../components/GardenProfile'
-import Trade from '../components/Trade'
+import ViewPostTrade from '../components/ViewPostTrade'
+import NewPostTrade from '../components/NewPostTrade'
 
 
 export default(
@@ -16,7 +17,10 @@ export default(
         <Route exact path='/editform' component={ EditForm } />
         <Route exact path='/post/:id' component={ ViewPost } />
         <Route exact path='/viewpost' component={ ViewPost } />
+        <Route exact path='/viewposttrade' component={ ViewPostTrade } />
+        <Route exact path='/newposttrade' component={ NewPostTrade } />
+
         <Route exact path='/gardenprofile/:id'  component={ GardenProfile } />
-        <Route exact path='/trade'  component={ Trade } />
+   
     </Switch> 
 )
