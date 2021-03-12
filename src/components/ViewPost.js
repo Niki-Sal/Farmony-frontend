8fa92aa0e7c48fc6e4ac3489efbaf9a315ed4a7c
mbrown <tugtuj@gmail.com>
2021-03-12 12:21:33 -0500

    Comment component add

--- src/components/ViewPost.js
+++ src/components/ViewPost.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import HolisticModel from '../models/holistic'
 import NewPost from './NewPost'
+import Comments from './Comments'
 
 
 const ViewPost = (props) => {
     const [post, setPost] = useState([])
     const [posts, setPosts] = useState([])
+    const [comments, setComments] = useState([])
     
     let thisPost = props.match.params.id
  
-    useEffect(async() => {
+    useEffect(async () => {
         
         const res = await HolisticModel.all() 
         setPosts(res.data) 
        
-        let onePost = res.data.filter((singlePost) => {
+        let onePost = await res.data.filter((singlePost) => {
             return thisPost === singlePost._id
         })
-        console.log(onePost)
+
+        let commentArray = await onePost[0].comment
+        console.log('************* ====> One Post', onePost)
+        console.log('************* ====> One Post', commentArray)
         setPost(onePost)
+        setComments(commentArray)
         
     }, []);
     
@@ -30,14 +36,29 @@ const ViewPost = (props) => {
         {post.name}
         {post.title}
         {post.content}
-        {post.comments}
         </div>  
         )
     })
-    
+
+    const aComment = comments.map((comment) => {
+        return(
+           <div key={comment._id}>
+        {comment.name}
+        {comment.date}    
+        {comment.content}    
+        </div> 
+        )
+    })
+    if (post.length > 0) {
+
+        console.log(post[0]._id)
+    }
     return (
         <div>
            {aPost}
+           <h4>Comments</h4>
+           {aComment}
+           <Comments id={ post.length > 0 ? post[0]._id : '' } />
         </div>
         
     );
