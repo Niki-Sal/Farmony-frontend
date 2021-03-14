da6987c1ee778a5823f1cc500d7d1cdd72641e9f
mbrown <tugtuj@gmail.com>
2021-03-13 19:45:57 -0500

    Add classes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Zipcode from './Zipcode'
 
 import {  Link } from 'react-router-dom';
+import Footer from './Footer'
 
 
 const Home = () => {
@@ -10,8 +11,13 @@ const Home = () => {
     return (
         
         <div class='HomepageMain'>
-            
+            <h1 className="welcome">Welcome to Farmony</h1>
+            <div class ="topDivHP"></div>
+            <div class ="bottomDivHP"></div>
+            <div class ="triangleDivHpR"></div>
+            <div class ="triangleDivHpL"></div>
             <Zipcode />
+            <Footer />
         </div>
     );
 }
