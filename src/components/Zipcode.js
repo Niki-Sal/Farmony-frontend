a3798c03203d0a2eef033f6329c8745e452a7afd
mbrown <tugtuj@gmail.com>
2021-03-14 18:46:14 -0400

    Class add for styling

--- src/components/Zipcode.js
+++ src/components/Zipcode.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { useState} from 'react'
+import { useState } from 'react'
 import GardenModel from '../models/garden'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Zipcode = () => {
     const [zipcode, setZipcode] = useState('')
     const [gardens, setGardens] = useState([])
-    
+
     const listOfGardens = gardens.map((gardenData, index) => {
         return (
-            <div key={gardenData._id}> 
-            <div className="garden-img-div"><img className="garden-img" src={gardenData.image} alt="garden"/></div>
-            <p>{gardenData.name} {gardenData.address} {gardenData.state} {gardenData.zip}</p>
-            <Link to={`/gardenprofile/${gardenData._id}`}>View Garden Profile</Link>
+            <div key={gardenData._id} className="gardenDisplayDiv">
+                <div className="garden-img-div"><img className="garden-img" src={gardenData.image} alt="garden" /></div>
+                <p className="gardenDisplayP">{gardenData.name}<br />{gardenData.address}<br />{gardenData.state} {gardenData.zip}</p>
+                <p className="gardenDisplayLink">
+                    <Link to={`/gardenprofile/${gardenData._id}`}>View Garden Profile</Link>
+                </p>
             </div>
         )
     })
-   
+
     const entry = (e) => {
         setZipcode(e.target.value)
         console.log('====> from entry', zipcode)
@@ -31,16 +33,16 @@ const Zipcode = () => {
         fetchData()
         console.log('====> from submit', zipcode)
     }
-   //Did you mean <= 1?
+    //Did you mean <= 1?
     return (
-        <div className="zipFormDiv">
-           
+        <div className="pageContainer">
+            
 
             <form onSubmit={handleSubmit} className="zip-form">
                 <input type="text" name="zip" placeholder="Enter zipcode to find farms near you." onChange={entry} className="form-controlHp" />
                 <button type="submit" id="zipButton">Search</button>
             </form>
-            
+
             {listOfGardens.length >= 1 ? listOfGardens : null}
         </div>
     )
