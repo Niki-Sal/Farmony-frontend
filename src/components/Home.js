08101dd8ad968af2231b56f4cb953672f639143a
mbrown <tugtuj@gmail.com>
2021-05-26 00:02:11 -0400

    Needs homepage styling to be fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,13 @@ const Home = () => {
 
     return (
 
-        <div class='HomepageMain'>
+        <div class='homepage-main'>
             <Video />
-            <div className="zipFormDiv">
-                <p className="aboutFarmony">About Farmony: <br />
-                The health and wellness industry is currently valued at 4.5 trillion dollars for a reason.  It’s hard not to notice that being healthy today seems to come with a hefty price tag. The purpose of Farmony is to ensure that no one has to go broke to get healthy. <span id="aboutFarmony2">Farmony is a social media website for those looking to cut the middle man out of their shopping and buy locally, straight from the source.  Users can buy or trade products with one another, offer or seek help at urban farms, trade recipes or just learn about the farmers in their area.
+            <div class="separator"></div>
+            <div class="home-about-container">
+               <div className="zipFormDiv">
+                <p className="aboutFarmony">
+                The health and wellness industry is currently valued at 4.5 trillion dollars for a reason.  It’s hard not to notice that being healthy today seems to come with a hefty price tag. The purpose of Farmony is to ensure that no one has to go broke to get healthy. <br /> <span id="aboutFarmony2">Farmony is a social media website for those looking to cut the middle man out of their shopping and buy locally, straight from the source.  Users can buy or trade products with one another, offer or seek help at urban farms, trade recipes or just learn about the farmers in their area.
             </span></p>
             </div>
 
@@ -27,7 +29,9 @@ const Home = () => {
             <div class="triangleDivHpL"></div>
             <Zipcode />
             <Team />
-            <Footer />
+            <Footer /> 
+            </div>
+            
         </div>
     );
 }
